9c219888b41ad2932933798a3dd676090de13e97
Viktor Nova <viktor@pinknoiseboys.com>
2015-02-19 10:36:25 +0000

    Cleaned up the script

--- freqncurses.sh
+++ freqncurses.sh
@@ -8,14 +8,22 @@ FIFO="fakempd.fifo"
 # This should probably be in some kind of ramdisk, or you might hurt your drive/card
 FIFO_PATH="."
 
-# Full path to the command that takes the audio you want to visualize and spits it out into a file
+# Command that takes the audio you want to visualize and spits it out into a file
 # e.g. jack-capture, arecord, ffmpeg, sox, vlc, etc
 CAPTURE_COMMAND="arecord"
-CAPTURE_OPTIONS="--rate=48000 -Dplug:default --disable-channels"
-# --disable-resample
 
-# Full path to the command that runs the visualizer
-VISUALIZER="/usr/local/bin/nausea"
+# Options for the capture command
+
+# Note: you should make sure you are sampling audio at the soundcard's native sample rate,
+# or you will get lots of noise in your visualizer and your device will hate you
+# If you are using arecord, the option is "--rate=48000", or your samplerate.
+# If you don't know your native samplerate, you can find out which ones your card supports by running:
+# "arecord --disable-resample" and then quickly hitting Ctrl+C (there is probably a better way)
+CAPTURE_OPTIONS=" --rate=48000 -Dplug:default --disable-resample"
+# --disable-channels"
+
+# Command that runs the visualizer
+VISUALIZER="nausea"
 VISUALIZER_OPTIONS="-c"
 
 
@@ -27,13 +35,5 @@ $VISUALIZER $VISUALIZER_OPTIONS $FIFO_PATH/$FIFO
 rm $FIFO_PATH/$FIFO
 
 
-
-# mkfifo fakempd.fifo
-# arecord > fakempd.fifo &
-# nausea -c fakempd.fifo
-
-
-
-
-
-
+# To-do: put this file in ram and make a loop that zeroes out the fifo every so often so ram doesn't get 
+# consumed by all the rock
